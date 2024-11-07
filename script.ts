719c68ce7ce25c6569f229150f5b9a1047435921
AhshamAlam <ahshamalam24@gmail.com>
2024-11-07 20:29:19 +0500

    Initial commit

---
+++ script.ts
@@ -0,0 +1,20 @@
+// for the more info button
+const moreInfoBtn = document.getElementById("more-info-btn");
+const extraInfo = document.getElementById("extra-info");
+
+moreInfoBtn?.addEventListener("click", () => {
+    extraInfo?.classList.toggle("hidden");
+});
+
+
+// for the email form to check for @
+const form = document.querySelector("form");
+const emailInput = document.getElementById("email") as HTMLInputElement;
+
+form?.addEventListener("submit", (e) => {
+    if (!emailInput.value.includes("@")) {
+        e.preventDefault();
+        alert("Please enter a valid email address")
+    }
+});
+
